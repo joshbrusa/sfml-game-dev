0a328a5bdb550f63cc1bbaf5b03959e5b3da454c
joshbrusa <joshbrusa@gmail.com>
2023-10-18 20:20:12 -0700

    some additions

--- chapter3/src/main.cpp
+++ chapter3/src/main.cpp
@@ -1,51 +1,16 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include <ResourceHolder.hpp>
-
-namespace Textures
-{
-    enum ID
-    {
-        Landscape,
-        Airplane,
-    };
-}
+#include <stdexcept>
+#include <Book/Game.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "Resources");
-    window.setFramerateLimit(20);
-
-    // load resources
-    ResourceHolder<sf::Texture, Textures::ID> textures;
     try
     {
-        textures.load(Textures::Landscape, "../assets/textures/Desert.png");
-        textures.load(Textures::Airplane, "../assets/textures/Eagle.png");
+        Game game;
+        game.run();
     }
-    catch (std::runtime_error &e)
+    catch (std::exception &e)
     {
-        std::cout << "Exception: " << e.what() << std::endl;
-        return 1;
-    }
-
-    // resources
-    sf::Sprite landscape(textures.get(Textures::Landscape));
-    sf::Sprite airplane(textures.get(Textures::Airplane));
-    airplane.setPosition(200.f, 200.f);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::KeyPressed || event.type == sf::Event::Closed)
-                return 0;
-        }
-
-        window.clear();
-        window.draw(landscape);
-        window.draw(airplane);
-        window.display();
+        std::cout << "\nEXCEPTION: " << e.what() << std::endl;
     }
 }
